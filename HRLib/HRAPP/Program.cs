906221a6add776d464fff685355fb1622e58b5ea
Himanshu Gupta <43908946+101guptaji@users.noreply.github.com>
2021-06-10 21:30:30 +0530

    Confirm employee and Trainee details with salary.
    Start new Project as Class Library. Name it as HRLib
    
    Create Employee Class
    Employee
    	String Name - property
    	String Address – property
    Total Constructor : 2
    Define 	Constructor with parameters (2 Parameter)
    
    Define abstract method CalculateSalary return double takes no parameter
    
    
    
    Write sub class ConfirmEmployee. Derived from Employee
    
    Variables / Properties
    	Double	 Basic 
    	String Designation 
    
    Total no of constructors : 2
    
    Define parameterized constructor (4 arguments)
    
    Override CalculateSalary method which return NetSalary
    This method cannot be “override” by child class
    
    Logic of CalculateSalary
    
    If Basic >= 30000
    	Hra – 30% of basic
    	Conv – 30 % of basic
    	PF – 10% of basic
    
    If Basic >= 20000
    	Hra – 20% of basic
    	Conv – 20 % of basic
    	PF – 10% of basic
    
    Else
    	Hra – 10% of basic
    	Conv – 10 % of basic
    	PF – 10% of basic
    
    NetSalary = basic + Hra + Conv - pf
    
    
    
    
    
    
    
    Define Trainee class. Derived from Employee
    Properties
    	Int 		No of Days
    	Double 	Rate per day
    
    No of constructors : 2
    
    Define parameterized constructor (4 parameters)
    
    Overrride CalculateSalary method
    
    Logic for the method
    
    NoOfDays * Rate
    
    
    •	Override ToString() method in all classes
    
    •	Create Empno as a int property which is auto generated unique value (means you will not take this value from the user but your Employee class  internally generate and assign unique value)
    
    •	Exception Handling
    Keep check that for Confirm Employee minimum basic is Rs. 5000 
    If less throw User Defined Exception
    
    -------------------------------------------------
    
    
    Create Console Application. Name it as HRAPP and test all 2 classes by creating object and call the method

---
+++ HRLib/HRAPP/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRLib;
+
+namespace HRAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfirmEmployee e1 = new ConfirmEmployee("Himanshu", "Jaipur, Raj", 50000, "Junior Developer");
+            Console.WriteLine(e1);
+            try
+            {
+                Console.WriteLine("Salary: " + e1.CalculateSalary());
+            }
+            catch(MinimumBasic ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            ConfirmEmployee e2 = new ConfirmEmployee();
+            e2.Name = "Ravi Sinha";
+            e2.Address = "Alwar, Rajasthan";
+            e2.Basic = 20000;
+            e2.Designation = "Intern";
+            Console.WriteLine(e2);
+
+            try
+            {
+                Console.WriteLine("Salary: " + e2.CalculateSalary());
+            }
+            catch (MinimumBasic ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+
+            Trainee t1 = new Trainee("Shunder", "Ranchi, India", 20, 5);
+            Console.WriteLine(t1);
+
+            Console.WriteLine("Salary: "+t1.CalculateSalary());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HRLib/HRLib/Employee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLib
+{
+    public abstract class Employee
+    {
+
+        #region FIELDS and PROPERTIES
+
+        private static int empNo=0;
+
+        public static int EmpNo
+        {
+            get { return empNo; }
+            set { empNo = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+
+        #endregion
+
+        #region Constructor
+        public Employee()
+        {
+            EmpNo++;
+        }
+
+        public Employee(string name, string address): this()
+        {
+            Name = name;
+            Address = address;
+        }
+
+        #endregion
+        #region METHODS
+
+        #endregion
+
+        #region METHODS
+        public abstract double CalculateSalary();
+
+        public override string ToString()
+        {
+            return string.Format($"Employee id: {EmpNo}, Name: {Name}, Address: {Address}");
+        }
+
+
+        #endregion
+    }
+}
